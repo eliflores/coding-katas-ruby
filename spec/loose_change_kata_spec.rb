c8cd8088cac9a2dff20c5a0c5f638d0205e1968f
mignonnesaurus <eli@mignonnesaurus.com>
2021-02-01 21:52:34 +0100

    Complete Kata :tada:

--- spec/loose_change_kata_spec.rb
+++ spec/loose_change_kata_spec.rb
@@ -1,15 +1,49 @@
 # frozen_string_literal: true
 
-[
-  { 29 => { 'Pennies' => 4, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 1 } },
-  { 29 => { 'Pennies' => 4, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 1 } },
-  { 29 => { 'Pennies' => 4, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 1 } },
-  { 29 => { 'Pennies' => 4, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 1 } },
-  { 29 => { 'Pennies' => 4, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 1 } }
-].each do |test_case|
-  context "Given #{test_case.keys.first} amount of US currency in cents" do
-    it 'Returns the expected loose change' do
-      expect (loose_change(test_case.keys.first)).to eq test_case.values
+require_relative '../loose_change_kata'
+
+describe 'Loose Change Kata' do
+  context 'Given an amount equal to 0 cents' do
+    it 'returns all coins equal to 0' do
+      expect(loose_change(0)).to eq(
+        { 'Pennies' => 0, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 0 }
+      )
+    end
+  end
+
+  context 'Given a negative amount of cents' do
+    it 'returns all coins equal to 0' do
+      expect(loose_change(-2)).to eq(
+        { 'Pennies' => 0, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 0 }
+      )
+      expect(loose_change(-435)).to eq(
+        { 'Pennies' => 0, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 0 }
+      )
+    end
+  end
+
+  context 'Given a float amount of cents' do
+    it 'rounds the amount down to the next integer' do
+      expect(loose_change(3.9)).to eq(
+        { 'Pennies' => 3, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 0 }
+      )
+      expect(loose_change(4.935)).to eq(
+        { 'Nickels' => 0, 'Pennies' => 4, 'Dimes' => 0, 'Quarters' => 0 }
+      )
+    end
+  end
+
+  context 'Given a integer amount of cents' do
+    it 'returns the least amount of coins' do
+      expect(loose_change(56)).to eq(
+        { 'Nickels' => 1, 'Pennies' => 1, 'Dimes' => 0, 'Quarters' => 2 }
+      )
+      expect(loose_change(29)).to eq(
+        { 'Pennies' => 4, 'Nickels' => 0, 'Dimes' => 0, 'Quarters' => 1 }
+      )
+      expect(loose_change(91)).to eq(
+        { 'Pennies' => 1, 'Nickels' => 1, 'Dimes' => 1, 'Quarters' => 3 }
+      )
     end
   end
 end
